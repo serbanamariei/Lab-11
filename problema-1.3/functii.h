b18359fea616743ed5c326eb677430ceaee675f0
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 20:10:41 +0300

    Create functii.h

---
+++ problema-1.3/functii.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+struct _DATA{
+	int zi;
+	int luna;
+	int an;
+	int ziAn;
+};
+typedef struct _DATA DATA;
+
+void conversie1(DATA *d);
+void conversie2(DATA *d);
+
+#endif
